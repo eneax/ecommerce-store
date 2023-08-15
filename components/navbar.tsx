137b8d3d375dbf1206604f4c67fc5e4aee448d43
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-15 20:09:50 +0200

    create basic navbar actions component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import getCategories from "@/actions/get-categories";
 
 import Container from "@/components/container";
 import MainNav from "@/components/main-nav";
+import NavbarActions from "@/components/navbar-actions";
 
 export const revalidate = 0;
 
@@ -17,8 +18,8 @@ export default async function Navbar() {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
-
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
