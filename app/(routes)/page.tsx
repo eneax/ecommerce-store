41cfb734d7a16049eeb244d85ca1526fc3d9d7d6
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-08-15 20:25:14 +0200

    add Billboard in the homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,18 @@
-export default function HomePage() {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+
+import Billboard from "@/components/billboard";
+import Container from "@/components/container";
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const billboard = await getBillboard("b448c9d9-61d9-4696-abe3-af01c7f77450");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 }
